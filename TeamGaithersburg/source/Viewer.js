8b1b44f3402aba94666b2e16225e8b63ebf3c13d
Aaron Rosenzweig <aaron@chatnbike.com>
2015-11-17 11:29:23 -0500

    Added ability to deploy
    Signed-off-by: Aaron Rosenzweig <aaron@chatnbike.com>

--- TeamGaithersburg/source/Viewer.js
+++ TeamGaithersburg/source/Viewer.js
@@ -17,7 +17,7 @@ enyo.kind({
 			{kind: "List", fit: true, touch: true, onSetupItem: "setupItem", components: [
 				{name: "item", ontap: "itemTap", classes: "panels-sample-flickr-item enyo-border-box", components: [
 					/*{name: "thumbnail", kind: "Image", classes: "panels-sample-flickr-thumbnail"},*/
-					{name: "title", classes: "panels-sample-flickr-title"}
+					{name: "title", allowHtml: true, classes: "panels-sample-flickr-title"}
 				]},
 				/*
 				{name: "more", components: [
@@ -149,7 +149,7 @@ enyo.kind({
 				"<p>Aaron Rosenzweig <a href=\"mailto:aaron@chatnbike.com\">aaron@chatnbike.com</a> - I'm a working family man who never really cared about city politics until he was directly impacted by it, now I know too much about the realities of our local government. The city threatened to take our children away with child protective services because a wealthy citizen didn't like our choice of pets. I refuse to sit idly by while our way of life is ruined. I'm not chicken to speak the truth, even when it hurts. <a href=\"https://www.change.org/p/city-of-gaithersburg-maryland-lift-rooster-ban\">Learn my story</a></p>"
 			}, 
 			
-			{title: "Fix our schools - Overcrowded and 150% overcapacity.",
+			{title: "Fix our schools",
 				details: "<p>Overcrowded and 150% overcapacity. New housing development continues. This is hurting our private housing values and our children deserve better.</p>" + 
 				"<p>In 2015 our local government voted to increase our overcapacity limits from 110% to 150% so that more houses can be built. This means 1/3 of students will be in temporary structures outside of the main school building. It's very shortsighted. The city gets immediate revenue from more citizens moving in which creates more tax dollars for them to spend. In the long run our house values will be hurt because good families do not want to move into a city with overcrowded schools.</p>" + 
 				"<p>It is obvious that our local government is either unwilling or unable to improve our schools. Something has to change.</p>" + 
@@ -159,8 +159,8 @@ enyo.kind({
 				"</ol>"			
 			}, 
 			
-			{title: "Stop CCT - not so rapid transit",
-				details: "<p>The CCT (Corridor Cities Transitway) will raise our taxes, create more congestion, remove homes through eminent domain and worse - yet we are doing nothing to stop it. We already have Ride-On buses that cover these routes that are underutilized</p>" + 
+			{title: "Stop Corridor Cities Transitway",
+				details: "<p>The CCT (Corridor Cities Transitway) is a &ldquo;not so rapid transit&rdquo; which will raise our taxes, create more congestion, remove homes through eminent domain and worse - yet we are doing nothing to stop it. We already have Ride-On buses that cover these routes that are underutilized</p>" + 
 				"<p>The idea is to connect Shady Grove to Clarksburg by messing up Gaithersburg. Muddy branch will be widened and Great Seneca Highway will become further congested by these busses and additional traffic lights on what is already a pickle point.</p>" +
 				"<p>No citizen wants the CCT, it is a power play by developers and politicians. First the CCT must be built and then Johns Hopkins will destroy Belward Farm on Muddy Branch. Our neighbor whose house will be taken during the widening of Muddy Branch is now stuck and cannot retire! They told him they would take his house in 2016, then they said 2018, now they say they are not sure when because they have no money. He has no way to sell his home and continue his life.</p>" + 
 				"<p>To finish this project the county will have to raise taxes. They have tried to create a special task force called the &ldquo;Independent Transit Authority&rdquo; which would have the authority to create whatever tax hikes they see fit without a public hearing.</p>" + 
@@ -171,7 +171,7 @@ enyo.kind({
 				"</ol>"			
 				},
 
-			{title: "Olde Towne needs love", 
+			{title: "Revitalize Olde Towne", 
 				details: "Not enough love in revitalizing our Olde Towne. Local government says they’ll improve Olde Towne but always go for new development instead." 
 			}, 
 			
@@ -181,12 +181,12 @@ enyo.kind({
 				"<p> How do we want our city to feel, what quality of life is important to us? This is a conversation we must have before it is too late</p>"
 			}, 
 
-			{title: "Dog Park repurposed for something else", 
+			{title: "Dog Park repurposed", 
 				details: "<p>Our local government changed the rules for our dog park without involving our citizens. Their end game is to reduce usage so that they can &rdquo;repurpose it for something else.&ldquo;</p>" +
 				"<p>Friendships were broken. People who used to go to the park were kicked out. I guess we are just running out of green space. Listen to how the City Manager says: &ldquo;usage just goes down so we can repurpose the dog park for something else&rdquo;: <a href=\"https://www.youtube.com/watch?v=j-f3fbdjIgQ#t=3m33s\">watch the video</a>"
 			}, 
 
-			{title: "Animal Control is out of control", 
+			{title: "Control &ldquo;Animal Control&rdquo;", 
 				details: "<p>City resources are being used to harass citizens but local government has done nothing to prevent it. If you don’t like someone, you can use taxpayer resources to wreak havoc on the innocent.</p>" + 
 				"<p>Have a parking spot that you fight over with your neighbor? Do you have a dog? Do you have kids? If you answered &ldquo;Yes&rdquo; to those questions than watch out. According to Lisa Holland, the director of Animal Control, your neighbor will say your dog bit them or that you spank your kids and the city will &ldquo;take care of it because that is what we do.&rdquo;</p>" + 
 				"<p>Please, don't be scared, if you have been harassed by the city please step forward and share your story. We can put an end to this type of abuse.</p>" +
@@ -195,7 +195,7 @@ enyo.kind({
 				
 			}, 
 
-			{title: "Tiny House Legislation", 
+			{title: "Tiny House Legalization", 
 				details: "<p>Across the country Americans are learning they can create their own dream homes on a small scale. They create pride of ownership and live debt-free. Local government prevents our citizens this freedom out of spite and ignorance.</p>" + 
 				"<p>Zuccarelli built a tiny house in her 0.5 acre property but the City condemned it because of zoning. They say it is illegal to have two single family homes in one plat of land. What is the purpose of this zoning? Zucarrelli's little house doesn't compete with the landscape and isn't even visible from the road. Is this just a power play, preventing Zuccarelli the freedom of using her land to the fullest? We think so. " + 
 				"Learn more: <ol>" +
